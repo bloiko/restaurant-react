5adb7a2dfb82d1060966142829c364327ea4d9f2
romanloiko <roman.loiko@perfsol.tech>
2023-03-04 19:01:23 +0200

    qweqwe

---
+++ src/pages/admin/admin.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import { MDBBtn } from 'mdb-react-ui-kit';
+import {Layout} from "../../components/Layout";
+import {Buffer} from 'buffer';
+
+import axios from "axios"
+
+const url = "http://localhost:8083/report/orders/month"
+
+const toBase64 = (response) =>
+    `data:${response.headers["content-type"]};base64,${Buffer.from(response.data, "binary").toString(
+        "base64"
+    )}`
+
+const handleGetReport1 = async () => {
+    axios.get(url, { responseType: 'arraybuffer' })
+        .then(response => {
+            console.log(toBase64(response))
+        });
+}
+
+export const Admin = () => {
+    const [file, setFile] = useState("")
+
+    const handleGetReport1 = async () => {
+        axios.get(url, { responseType: 'arraybuffer' })
+            .then(response => {
+                setFile(toBase64(response))
+                console.log(toBase64(response))
+            });
+    }
+
+    return (
+        <Layout>
+            <MDBBtn onClick={handleGetReport1}>Button</MDBBtn>
+            {file}
+        </Layout>
+    );
+};
+
